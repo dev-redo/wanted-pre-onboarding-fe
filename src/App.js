3e999ae141b92e185362e4dca57882b8ce375f6d
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-16 22:26:29 +0900

    Feat: Login 경로 설정
    Login 페이지 경로를 설정하였습니다.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Login from './pages/Login';
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  );
 }
 
 export default App;
