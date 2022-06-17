f04603d33999a5c1863d4eab1701275db00d92a6
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-17 12:02:09 +0900

    Refactor: useForm을 useLoginValidator로 구체화
    폼 입력창의 유효성을 검사해주는 useForm을 Login에 대해서 한정하는 useLoginValidator로 구체화
    하였다. useLoginValidator은 각 입력창의 유효성검사와 렌더링까지 담당한다.

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
 import LoginForm from './LoginForm';
-import useForm from '../../hooks/useForm';
-import { validator } from '../../util/checkInputValidate';
-
-const inputRegex = {
-  email:
-    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*[a-zA-Z]{2,3}$/,
-  password:
-    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-};
+import useLoginValidator from '../../hooks/useLoginValidator';
 
 export default function LoginFormLayout() {
-  const { values, valuesChangeHandler, error, isAllValid } = useForm({
-    initValues: { email: '', password: '' },
-    validator,
-    inputRegex,
-  });
-
+  const [isSubmit, renderInputs] = useLoginValidator();
   return (
     <>
       <LoginForm>
-        <LoginForm.Input
-          values={values}
-          handlerChange={valuesChangeHandler}
-          error={error}
-        />
-        <LoginForm.Button isAllValid={isAllValid}>로그인</LoginForm.Button>
+        <LoginForm.Input renderInputs={renderInputs} />
+        <LoginForm.Button isSubmit={isSubmit}>로그인</LoginForm.Button>
       </LoginForm>
     </>
   );
