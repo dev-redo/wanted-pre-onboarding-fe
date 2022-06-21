5f700dbcb53ae996bc49e5db2069a76d80e7623a
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-22 04:21:44 +0900

    Feat: root height 100% 설정
    Spinner가 화면 중앙에 배치되도록 id가 root인 태그에 height를 100% 설정하였다.

--- src/styles/globalStyles.js
+++ src/styles/globalStyles.js
@@ -2,15 +2,22 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
- ${reset}
- * {
-   box-sizing:border-box;
-   outline:none;
-   border:none;
- }
- html, body {
+  ${reset}
+  * {
+    box-sizing:border-box;
+    outline:none;
+    border:none;
+  }
+  html, body, #root {
     height: 100%;
- }
+  }
+  a {
+    text-decoration:none;
+    color: black;
+  }
+  button {
+    cursor: pointer;
+  }
 `;
 
 export default GlobalStyles;
