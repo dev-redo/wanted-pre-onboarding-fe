ede0da75466fd8eb64aebc6a52c3eba12e41dce4
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-22 00:30:44 +0900

    Fix: 아이콘 버튼 width, height 사이즈 동적 스타일링
    아이콘 버튼 컴포넌트인 IconButton의 width와 height를 props로 받아 동적으로 스타일링하게끔 수정하였다.
    default 값으로 각각 2rem으로 설정하였다.

--- src/components/common/Atom/IconButton.jsx
+++ src/components/common/Atom/IconButton.jsx
@@ -11,7 +11,7 @@ const S = {};
 S.Button = styled.button`
   background-color: transparent;
   svg {
-    width: 2rem;
-    height: 2rem;
+    width: ${({ width }) => (width ? `${width}rem` : '2rem')};
+    height: ${({ height }) => (height ? `${height}rem` : '2rem')};
   }
 `;
