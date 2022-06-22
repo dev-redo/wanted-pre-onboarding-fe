f6b364f4fca36e08670276f3a3048ff4c2b1a4d7
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-23 04:30:45 +0900

    Feat: 인스타그램 feed 정보 fetch
    인스타그램 feed 정보가 담긴 json파일을 fetch하였다.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,19 +2,25 @@ import React from 'react';
 import { useRecoilValue } from 'recoil';
 import { isAuthorized } from '../modules/atoms/auth';
 import GlobalLayout from '../layout/global';
-import Feed from '../components/Feed';
+import { getAsyncValue } from '../lib/api/getAsyncValue';
+import FeedLayout from '../components/Feed';
 import { Navigate } from 'react-router-dom';
+import useHttp from '../hooks/useHttp';
+
+const suspendedFeedData = getAsyncValue(
+  useHttp({
+    url: './data/instargramFeedData.json',
+  }),
+);
 
 export default function Home({ children, ...props }) {
   const auth = useRecoilValue(isAuthorized);
+  const feedData = suspendedFeedData.get().data;
 
   return (
     <>
       {!auth && <Navigate to="/login" replace={true} />}
-      <GlobalLayout>
-        <Feed />
-        <Feed />
-      </GlobalLayout>
+      <GlobalLayout>{JSON.stringify(feedData)}</GlobalLayout>
     </>
   );
 }
