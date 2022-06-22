34a2769d2e76cce6124e4369f5a28eec023a3350
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-23 04:20:31 +0900

    Feat: 페이지 렌더링 중 에러 발생 시 Error Boundaries로 핸들링
    페이지 렌더링 중 발생한 에러를(ex. fetch 실패) 리액트의  Error Boundaries로 핸들링하였다.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
 import React, { Suspense, lazy } from 'react';
+import { ErrorBoundary } from 'react-error-boundary';
 import { Route, Routes } from 'react-router-dom';
 import Loader from './components/common/Loader';
+import Error from './components/common/Error';
 
 const Login = lazy(() => import('./pages/Login'));
 const Home = lazy(() => import('./pages/Home'));
 
 function App() {
   return (
-    <Suspense fallback={<Loader />}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Suspense>
+    <ErrorBoundary
+      fallback={<Error>페이지 로드 중 문제가 발생했습니다!</Error>}
+    >
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
+    </ErrorBoundary>
   );
 }
 
