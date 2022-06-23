9e272e64b42ffe8e855f66e6111ed1b650a91d9d
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-24 03:46:56 +0900

    Refactor: Login 데이터 관리를  리코일을 컨텍스트로 변경

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { RecoilRoot } from 'recoil';
+import { UserProvider } from './modules/context/auth';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import GlobalStyles from './styles/globalStyles';
@@ -8,11 +8,11 @@ import GlobalStyles from './styles/globalStyles';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <RecoilRoot>
+    <UserProvider>
       <GlobalStyles />
       <BrowserRouter>
         <App />
       </BrowserRouter>
-    </RecoilRoot>
+    </UserProvider>
   </React.StrictMode>,
 );
