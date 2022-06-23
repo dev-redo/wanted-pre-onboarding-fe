f16bccf048acdb8c251a184e6597da88ba8df49f
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-24 03:45:41 +0900

    Feat: Form 컴포넌트에 Props로 전달하는 Field data 분리

---
+++ src/constants/fieldData.js
@@ -0,0 +1,16 @@
+import { REGEX_EMAIL, REGEX_PW } from '../constants/regex';
+
+export const loginField = [
+  {
+    name: 'email',
+    placeholder: '전화번호, 사용자 이름 또는 이메일',
+    type: 'text',
+    regex: REGEX_EMAIL,
+  },
+  {
+    name: 'password',
+    placeholder: '비밀번호',
+    type: 'password',
+    regex: REGEX_PW,
+  },
+];
