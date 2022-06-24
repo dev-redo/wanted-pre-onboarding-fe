8a2e50d0baa0c5d08df1ea32d00f56d4ac5c6d9e
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-24 18:22:48 +0900

    Bug: 숫자 표현 에러나는 문제 해결
    toLocaleString 메소드를 잘못 사용해 에러난 문제를 해결하였습니다.

--- src/util/convertNationalNumbers.js
+++ src/util/convertNationalNumbers.js
@@ -4,12 +4,12 @@
  * @function 각 나라의 숫자 표현 방식으로 변환
  * @param {number} 변환할 숫자
  * @param {string} 변환하고자하는 나라
- * @return {number} 각 나라 표현으로 변환된 숫자
+ * @return { string } 각 나라 표현으로 변환된 문자
  */
 export function convertNationalNumbers({ num, country }) {
   const convertNum = country
-    ? num.toLocaleString(country)
-    : num.toLocaleString();
+    ? Number(num).toLocaleString(country)
+    : Number(num).toLocaleString();
 
-  return Number(convertNum);
+  return convertNum;
 }
