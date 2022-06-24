f04e7b14b26a3b39c76f34a928634614baded373
hsy <69149030+dev-redo@users.noreply.github.com>
2022-06-25 07:29:31 +0900

    Style: jsDoc 수정

--- src/hooks/useLazyImageObserver.jsx
+++ src/hooks/useLazyImageObserver.jsx
@@ -4,7 +4,7 @@ import { useState, useRef, useEffect } from 'react';
  * useLazyImageObserver
  * @typedef { function }
  * @function Intersection Observer를 이용해 레이지 로딩을 구현하는 훅
- * @return { src, ref }
+ * @return { src, ref } 인자로 전달받은 src로 업데이트 된 상태, 구독하는 타겟 엘레먼트
  */
 export function useLazyImageObserver({ src }) {
   const [imageSrc, setImageSrc] = useState(null);
