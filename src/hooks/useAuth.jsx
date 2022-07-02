16e48a8132258afcb5736f4fd2e4554eaead1e29
hsy <69149030+dev-redo@users.noreply.github.com>
2022-07-03 00:07:21 +0900

    Style: domain 함수 경로 변경
    Login시 사용되는 도메인 함수들을 각 컴포넌트 내의 domian 폴더에서 util폴더로 이동시켰다.
    따라서 해당 함수들의 경로들을 수정해주었다.

--- src/hooks/useAuth.jsx
+++ src/hooks/useAuth.jsx
@@ -1,14 +1,34 @@
 import { useNavigate } from 'react-router-dom';
 import { useUserDispatch } from '../modules/context/auth/provider';
 import { getRegisterUserList } from '../api/auth';
-import { extractIdFromEmail } from '../components/LoginForm/domain/extractIdFromEmail';
-import { findUser } from '../components/LoginForm/domain/findUser';
+import { checkIsUserExist } from '../util/checkIsUserExist';
+import { extractIdFromEmail } from '../util/extractIdFromEmail';
 
-const getLoginCallback = (navigate, dispatch) => {
-  return async (isValid, inputValues) => {
+/**
+ * useAuth
+ * @typedef { function }
+ * @function login, logout 콜백함수를 모아두는 hook
+ * @return { function } login 콜백함수
+ * @return { function } logout 콜백함수
+ */
+export function useAuth() {
+  const navigate = useNavigate();
+  const dispatch = useUserDispatch();
+
+  const loginCallback = getLoginCallback(navigate, dispatch);
+  const logoutCallback = getLogoutCallback(navigate, dispatch);
+
+  return { loginCallback, logoutCallback };
+}
+
+const getLoginCallback =
+  (navigate, dispatch) => async (isValid, inputValues) => {
     if (!isValid) return;
     const registerUserList = await getRegisterUserList();
-    const isUserExist = await findUser(inputValues, registerUserList);
+    const isUserExist = await checkIsUserExist(
+      inputValues,
+      registerUserList,
+    );
 
     if (!isUserExist) {
       alert('아이디 또는 비밀번호를 잘못 입력하셨습니다!');
@@ -23,25 +43,12 @@ const getLoginCallback = (navigate, dispatch) => {
     alert('로그인 성공하였습니다.');
     navigate('/', { replace: true });
   };
+
+const getLogoutCallback = (navigate, dispatch) => async () => {
+  alert('로그아웃 되었습니다.');
+  dispatch({
+    type: 'LOGOUT',
+  });
+
+  navigate('/login', { replace: true });
 };
-
-const getLogoutCallback = (navigate, dispatch) => {
-  return async () => {
-    alert('로그아웃 되었습니다.');
-    dispatch({
-      type: 'LOGOUT',
-    });
-
-    navigate('/login', { replace: true });
-  };
-};
-
-export function useAuth() {
-  const navigate = useNavigate();
-  const dispatch = useUserDispatch();
-
-  const loginCallback = getLoginCallback(navigate, dispatch);
-  const logoutCallback = getLogoutCallback(navigate, dispatch);
-
-  return { loginCallback, logoutCallback };
-}
