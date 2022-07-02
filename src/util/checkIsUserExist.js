edd38366baabfb720e9477d603bd6c83ff2a1038
hsy <69149030+dev-redo@users.noreply.github.com>
2022-07-03 00:04:03 +0900

    Style: 로그인 Form 도메인 함수들을 util로 이동
    로그인 Form에서 사용되는 도메인 함수들이 추후 다른 컴포넌트에서도 사용될 것이라 판단해 util 폴더로 이동시켰다.

--- src/util/checkIsUserExist.js
+++ src/util/checkIsUserExist.js
@@ -1,12 +1,15 @@
 /**
- * findUser
- * @typedef { function }
- * @function findUser - 입력받은 values에 해당하는 사용자가 존재하는지 판별
+ * checkIsUserExist
+ * @typedef { checkIsUserExist }
+ * @function checkIsUserExist - 입력받은 values에 해당하는 사용자가 존재하는지 판별
  * @param { object } 입력된 email, password 객체
  * @param { json } 가입된 유저의 email, password 정보를 담은 JSON파일
  * @return { boolean } 일치하는 사용자가 존재하는지 여부
  */
-export async function findUser({ email, password }, registerUserList) {
+export function checkIsUserExist(
+  { email, password },
+  registerUserList,
+) {
   const isUserExist = registerUserList['data'].find(
     ({ userEmail, userPassword }) => {
       return email === userEmail && password === userPassword;
